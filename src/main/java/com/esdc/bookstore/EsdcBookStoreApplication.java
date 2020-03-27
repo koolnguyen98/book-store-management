e5e696c54562187d82fa0c8f0c39e5be0c246aaf
Kool nguyen <koolnguyen1610@gmail.com>
2020-03-27 19:15:56 +0700

    Create book store project management

---
+++ src/main/java/com/esdc/bookstore/EsdcBookStoreApplication.java
@@ -0,0 +1,13 @@
+package com.esdc.bookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsdcBookStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsdcBookStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/esdc/bookstore/EsdcBookStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esdc.bookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsdcBookStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
