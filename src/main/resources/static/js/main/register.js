6ec42280f088a85e4b0088dd8eb3c048683014f9
yunkishiness73 <yunkishiness73@gmail.com>
2020-05-12 17:45:14 +0700

    validate form

--- src/main/resources/static/js/main/register.js
+++ src/main/resources/static/js/main/register.js
@@ -1,9 +1,80 @@
+const isAccountValid = (payload) => {
+	const emailPattern = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
+	const phonePattern = /(03|07|08|09|01[2|6|8|9])+([0-9]{8})\b/;
+	
+	const { username, password, confPass, fullname,
+		email, phoneNumber, address, sex } = payload;
+		
+	let formIsValid = true;
+	
+	$(".username-helper").html('');
+	$(".password-helper").html('');
+	$(".confPassword-helper").html('');
+	$(".fullName-helper").html('');
+	$(".email-helper").html('');
+	$(".phoneNumber-helper").html('');
+	$(".address-helper").html('');
+	
+	if (username.trim().length == 0) {
+		$(".username-helper").html('Username is required');
+		formIsValid = false;
+	}
+	
+	if (password.trim().length == 0) {
+		$(".password-helper").html('Password is required');
+		formIsValid = false;
+	}
+	
+	if (confPass.trim().length == 0) {
+		$(".confPassword-helper").html('Password is required');
+		formIsValid = false;
+	} else {
+		if (password.trim() !== confPass.trim()) {
+			$(".confPassword-helper").html('Password does not match');
+			formIsValid = false;
+		}
+	}
+	
+	if (fullname.trim().length == 0) {
+		$(".fullName-helper").html('Fullname is required');
+		formIsValid = false;
+	}
+	
+	if (email.trim().length == 0) {
+		$(".email-helper").html('Email is required');
+		formIsValid = false;
+	} else {
+		if (!emailPattern.test(email.trim())) {
+			$(".email-helper").html('Email is not valid');
+			formIsValid = false;
+		}
+	}
+	
+	if (phoneNumber.trim().length == 0) {
+		$(".phoneNumber-helper").html('Phone number is required');
+		formIsValid = false;
+	} else {
+		if (!phonePattern.test(phoneNumber.trim())) {
+			$(".phoneNumber-helper").html('Phone number is not valid');
+			formIsValid = false;
+		}
+	}
+	
+	if (address.trim().length == 0) {
+		$(".address-helper").html('Address is required');
+		formIsValid = false;
+	}
+	
+	
+	return formIsValid;
+}
+
 const register = () => {
 	$('body').on('click', '.register-btn', function(e) {
 		e.preventDefault();
 		
 		let username = $('#username').val();
-		let pass = $('#pass').val();
+		let password = $('#pass').val();
 		let confPass = $('#con-pass').val();
 		let fullname = $('#fullname').val();
 		let email = $('#email').val();
@@ -11,8 +82,18 @@ const register = () => {
 		let address = $('#address').val();
 		let sex = $('#male').is(':checked') ? 1 : 0;
 		
-		console.log(username, pass, confPass, fullname, email, phoneNumber, address, username, sex);
+		let payload = {
+			username,password,confPass,
+			fullname, email, phoneNumber, address, sex
+		};
 		
+		let isValid = isAccountValid(payload);
+		
+		if (isValid) {
+			alert('form is valid');
+		} else {
+			alert('form is not valid');
+		}
 	})
 }
 
